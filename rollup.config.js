f201faa615e8130a7d789e587c07d4d166444629
deepak <burpdeepak96@gmail.com>
2024-12-13 18:21:34 +0530

    initial version updated

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss'; // Import the plugin
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    postcss({
+      extract: true, // Extract CSS to a separate file
+      minimize: true, // Minify the CSS
+    }),
+  ],
+  external: ['react', 'react-dom'],
+};
